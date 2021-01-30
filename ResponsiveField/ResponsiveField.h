cbd6fb12f63236a0994ee231f51b6a90bc03cc9e
Carlos Modinez <modinez.carlos@gmail.com>
2021-01-30 12:42:10 -0300

    Initial Commit

---
+++ ResponsiveField/ResponsiveField.h
@@ -0,0 +1,18 @@
+//
+//  ResponsiveField.h
+//  ResponsiveField
+//
+//  Created by Carlos Modinez on 30/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResponsiveField.
+FOUNDATION_EXPORT double ResponsiveFieldVersionNumber;
+
+//! Project version string for ResponsiveField.
+FOUNDATION_EXPORT const unsigned char ResponsiveFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResponsiveField/PublicHeader.h>
+
+
